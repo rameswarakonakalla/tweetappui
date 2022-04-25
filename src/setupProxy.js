d5233afa1945f6ef0168bf8408d1dc53fc3ca551
ramesh2554 <konakalla.ramesh143@gmail.com>
2022-04-25 17:08:58 +0530

    first commit

---
+++ src/setupProxy.js
@@ -0,0 +1,30 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+ 
+module.exports = function (app) {
+  app.use(
+    createProxyMiddleware('/api1', {
+      // target: 'http://localhost:8082', // API endpoint 1
+      target: 'http://3.87.108.193:8082',
+      changeOrigin: true,
+      pathRewrite: {
+        "^/api1": "",
+      },
+      headers: {
+        Connection: "keep-alive"
+      }
+    })
+  );
+  app.use(
+    createProxyMiddleware('/api2', {
+      // target: 'http://localhost:8083', // API endpoint 2
+      target: 'http://34.229.167.216:8083',
+      changeOrigin: true,
+      pathRewrite: {
+        "^/api2": "",
+      },
+      headers: {
+        Connection: "keep-alive"
+      }
+    })
+  );
+}
